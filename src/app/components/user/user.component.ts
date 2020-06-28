f3f44b8516ce7b75a411cfdd724469d524fb0dba
Audrey M <masambaaudrey1@gmail.com>
2020-06-28 16:35:33 +0200

    Types

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -15,20 +15,49 @@ import { Statement } from '@angular/compiler';
 
 export class UserComponent {
   // Properties
-  firstName='John';
-  lastName = 'Smith';
-  age = 30;
-  address = {
-    street: '50 Main st',
-    city: 'Boston',
-    state: 'MA'
-  }
+  firstName: string;
+  lastName: string;
+  age: number;
+  address;
+
+  foo: any;
+  hasKids: boolean;
+  numberArray : number [];
+  stringArray: string [];
+  mixedArray: any;
+  myTuple: [string, number, boolean];
+  unusable: void;
+  u: undefined;
+  n: null;
+
   // Methods
   constructor(){ //Initialize the component //Injected dependencies
+    this.firstName = 'John';
+    this.lastName = 'Doe';
+    this.age = 30;
+    this.address ={
+      street: '50 Main st',
+      city: 'Boston',
+      state: 'MA'
+    };
+    this.foo= true;
+    this.hasKids= true;
+    this.numberArray= [1,2,3];
+    this.stringArray= ['hello', 'world'];
+    this.mixedArray= [true, undefined, 1,'hello'];
+    this.myTuple = ['hello',1, true];
+    this.unusable = undefined;
+    this.u = undefined;
+    this.n = null;
 
+    console.log(this.addNumbers(2,3));
   }
 
   showAge(){
     return this.age + 2;
+  }
+
+  addNumbers(num1:number, num2: number): number{
+    return num1 + num2
   }
 }
