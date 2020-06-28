02d4a2e3184e07dab56542716e9a0d4a9f806f83
Audrey M <masambaaudrey1@gmail.com>
2020-06-28 19:44:06 +0200

    structure directive : ngFor

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -1,18 +1,68 @@
 import { Component, OnInit } from '@angular/core';
 
+import { User } from '../../models/User'
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
   styleUrls: ['./users.component.scss']
 })
 export class UsersComponent implements OnInit {
+  users: User[];
+
 
   constructor() { //inhected
-
   }
 
+  
+  //Methods
   ngOnInit() { //ajx place or service
-    console.log('init...')
+    this.users = [
+      {
+        firstName: 'John',
+        lastName: 'Doe',
+        age: 30,
+        address: {
+          street: '50 Main st',
+          city: 'Boston',
+          state: 'MA',
+        }
+      },
+      {
+        firstName: 'Kevin',
+        lastName: 'lala',
+        age: 30,
+        address: {
+          street: '5 Los st',
+          city: 'Miami',
+          state: 'FL',
+        }
+      },
+      {
+        firstName: 'Karen',
+        lastName: 'Polo',
+        age: 15,
+        address: {
+          street: '2 california st',
+          city: 'Danemark',
+          state: 'MA',
+        }
+      }
+    ];
+
+    this.addUser({
+      firstName: 'David',
+      lastName: 'Jackson',
+      age: 44,
+      address: {
+        street: '12 Wake st',
+        city: 'Miami',
+        state: 'FL',
+      }
+    })
+  }
+
+  addUser(user: User) {
+    this.users.push(user);
   }
 
 }
