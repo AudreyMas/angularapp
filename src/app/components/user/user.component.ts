d15c61d3be17bb02d9be92af1b30f3d7281fe5c9
Audrey M <masambaaudrey1@gmail.com>
2020-06-28 15:29:25 +0200

    Creat new component: user component

---
+++ src/app/components/user/user.component.ts
@@ -0,0 +1,17 @@
+import {Component} from '@angular/core'
+
+@Component({
+  selector:'app-user',
+  templateUrl: './user.component.html',
+  styleUrls:['./User.component.scss'],
+  // template:'<h2>John Doe</h2>'
+  // styles:[`
+  //     h2 {
+  //       color:blue
+  //     }`
+  //   ]
+})
+
+export class UserComponent {
+  
+}
