3ea59ff2533e1d48c74d6f3dae7aa62873cc41b0
Audrey M <masambaaudrey1@gmail.com>
2020-07-01 00:10:26 +0200

    Activate route + page for each post

---
+++ src/app/components/post/post.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+import {Location} from '@angular/common';
+import {PostService} from '../../services/post.service';
+
+import {Post} from '../../models/Post';
+
+@Component({
+  selector: 'app-post',
+  templateUrl: './post.component.html',
+  styleUrls: ['./post.component.scss']
+})
+export class PostComponent implements OnInit {
+  // Properties
+  post: Post;
+
+  constructor(
+    private route: ActivatedRoute,
+    private postService: PostService,
+    private Location: Location,
+  ) { }
+
+  ngOnInit() {
+    const id= +this.route.snapshot.paramMap.get ('id');
+    // console.log(id)
+    this.postService.getPost(id).subscribe(post => this.post = post)
+  }
+
+}
